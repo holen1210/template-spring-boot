653bf4b12cd0cd460180700a383b4153bdc177cc
吕昊临 <lvhaolin1@corp.netease.com>
2018-11-30 16:56:27 +0800

    Refactor: 字段校验非空使用notblank更合适

--- src/main/java/org/holen/template/domain/vo/UserVO.java
+++ src/main/java/org/holen/template/domain/vo/UserVO.java
@@ -1,16 +1,16 @@
 package org.holen.template.domain.vo;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 /**
  * @author holen
  * @date 2018/11/20
  */
 public class UserVO {
-    @NotNull(message = "用户名不能为空")
+    @NotBlank(message = "用户名不能为空")
     private String username;
 
-    @NotNull(message = "手机号不能为空")
+    @NotBlank(message = "手机号不能为空")
     private Long phone;
 
     public String getUsername() {
